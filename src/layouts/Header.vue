ae653abe0b3cae48921e034294b787739082a3dd
isman17 <39393350+isman17@users.noreply.github.com>
2022-07-20 10:57:59 +0700

    Initial commit

---
+++ src/layouts/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div role="header">
+        <header>
+            <div class="wrapper">
+                <nav>
+                    <RouterLink to="/">Home</RouterLink>
+                    <RouterLink to="/about">About</RouterLink>
+                </nav>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+export default {
+    components: {
+        RouterLink,
+    }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script>
+import SwaggerUI from 'swagger-ui'
+import 'swagger-ui/dist/swagger-ui.css';
+
+export default {
+  mounted() {
+    SwaggerUI({
+        url: './src/assets/api.json',
+        dom_id: '#swagger'
+    })
+  }
+}
+</script>
+
+<template>
+  <main>
+    Home
+    <div id="swagger"></div>
+  </main>
+</template>
